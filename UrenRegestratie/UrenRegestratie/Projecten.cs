e6fc4a61d5e610aa7189322dfb09dc3b71896ec9
petervanhummel <petervanhummel@hotmail.com>
2014-09-24 14:34:28 +0200

    classes aangemaakt
    see above

---
+++ UrenRegestratie/UrenRegestratie/Projecten.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrenRegestratie
+{
+    public class Projecten
+    {
+
+        public int ProjectID { get; set; }
+
+        public string Projectnaam { get; set; }
+
+        public string Projectomschrijving { get; set; }
+
+        public DateTime Begindatum { get; set; }
+
+        public DateTime Einddatum { get; set; }
+
+        public bool Afgesloten { get; set; }
+
+    }
+}
