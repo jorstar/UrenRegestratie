e1ae0c94c1582904a479234fb09f34089d78db3c
petervanhummel <petervanhummel@hotmail.com>
2014-10-23 10:19:10 +0200

    m

--- UrenRegestratie/UrenRegestratie/NieuwProject.Designer.cs
+++ UrenRegestratie/UrenRegestratie/NieuwProject.Designer.cs
@@ -34,20 +34,12 @@
             this.label4 = new System.Windows.Forms.Label();
             this.txtProjectnaam = new System.Windows.Forms.TextBox();
             this.txtProjectomschrijving = new System.Windows.Forms.TextBox();
-            this.combBeginDag = new System.Windows.Forms.ComboBox();
-            this.combBeginMaand = new System.Windows.Forms.ComboBox();
-            this.txtBeginJaar = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.txtEindJaar = new System.Windows.Forms.TextBox();
-            this.combEindMaand = new System.Windows.Forms.ComboBox();
-            this.combEindDag = new System.Windows.Forms.ComboBox();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.btnOpslaan = new System.Windows.Forms.Button();
+            this.dateTimeBegin = new System.Windows.Forms.DateTimePicker();
+            this.dateTimeEind = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // label1
@@ -102,88 +94,6 @@
             this.txtProjectomschrijving.Size = new System.Drawing.Size(339, 86);
             this.txtProjectomschrijving.TabIndex = 5;
             // 
-            // combBeginDag
-            // 
-            this.combBeginDag.FormattingEnabled = true;
-            this.combBeginDag.Location = new System.Drawing.Point(87, 186);
-            this.combBeginDag.Name = "combBeginDag";
-            this.combBeginDag.Size = new System.Drawing.Size(51, 21);
-            this.combBeginDag.TabIndex = 6;
-            // 
-            // combBeginMaand
-            // 
-            this.combBeginMaand.FormattingEnabled = true;
-            this.combBeginMaand.Location = new System.Drawing.Point(160, 186);
-            this.combBeginMaand.Name = "combBeginMaand";
-            this.combBeginMaand.Size = new System.Drawing.Size(53, 21);
-            this.combBeginMaand.TabIndex = 7;
-            // 
-            // txtBeginJaar
-            // 
-            this.txtBeginJaar.Location = new System.Drawing.Point(235, 186);
-            this.txtBeginJaar.Name = "txtBeginJaar";
-            this.txtBeginJaar.Size = new System.Drawing.Size(66, 20);
-            this.txtBeginJaar.TabIndex = 10;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(144, 189);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(10, 13);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "-";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(219, 189);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(10, 13);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "-";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(219, 219);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(10, 13);
-            this.label6.TabIndex = 20;
-            this.label6.Text = "-";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(144, 219);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(10, 13);
-            this.label7.TabIndex = 19;
-            this.label7.Text = "-";
-            // 
-            // txtEindJaar
-            // 
-            this.txtEindJaar.Location = new System.Drawing.Point(235, 216);
-            this.txtEindJaar.Name = "txtEindJaar";
-            this.txtEindJaar.Size = new System.Drawing.Size(66, 20);
-            this.txtEindJaar.TabIndex = 18;
-            // 
-            // combEindMaand
-            // 
-            this.combEindMaand.FormattingEnabled = true;
-            this.combEindMaand.Location = new System.Drawing.Point(160, 216);
-            this.combEindMaand.Name = "combEindMaand";
-            this.combEindMaand.Size = new System.Drawing.Size(53, 21);
-            this.combEindMaand.TabIndex = 17;
-            // 
-            // combEindDag
-            // 
-            this.combEindDag.FormattingEnabled = true;
-            this.combEindDag.Location = new System.Drawing.Point(87, 216);
-            this.combEindDag.Name = "combEindDag";
-            this.combEindDag.Size = new System.Drawing.Size(51, 21);
-            this.combEindDag.TabIndex = 16;
-            // 
             // label9
             // 
             this.label9.AutoSize = true;
@@ -221,25 +131,31 @@
             this.btnOpslaan.UseVisualStyleBackColor = true;
             this.btnOpslaan.Click += new System.EventHandler(this.btnOpslaan_Click);
             // 
+            // dateTimeBegin
+            // 
+            this.dateTimeBegin.Location = new System.Drawing.Point(87, 189);
+            this.dateTimeBegin.Name = "dateTimeBegin";
+            this.dateTimeBegin.Size = new System.Drawing.Size(200, 20);
+            this.dateTimeBegin.TabIndex = 25;
+            // 
+            // dateTimeEind
+            // 
+            this.dateTimeEind.Location = new System.Drawing.Point(87, 210);
+            this.dateTimeEind.Name = "dateTimeEind";
+            this.dateTimeEind.Size = new System.Drawing.Size(200, 20);
+            this.dateTimeEind.TabIndex = 26;
+            // 
             // NieuwProject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(655, 285);
+            this.Controls.Add(this.dateTimeEind);
+            this.Controls.Add(this.dateTimeBegin);
             this.Controls.Add(this.btnOpslaan);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.txtEindJaar);
-            this.Controls.Add(this.combEindMaand);
-            this.Controls.Add(this.combEindDag);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.txtBeginJaar);
-            this.Controls.Add(this.combBeginMaand);
-            this.Controls.Add(this.combBeginDag);
             this.Controls.Add(this.txtProjectomschrijving);
             this.Controls.Add(this.txtProjectnaam);
             this.Controls.Add(this.label4);
@@ -262,19 +178,11 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtProjectnaam;
         private System.Windows.Forms.TextBox txtProjectomschrijving;
-        private System.Windows.Forms.ComboBox combBeginDag;
-        private System.Windows.Forms.ComboBox combBeginMaand;
-        private System.Windows.Forms.TextBox txtBeginJaar;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox txtEindJaar;
-        private System.Windows.Forms.ComboBox combEindMaand;
-        private System.Windows.Forms.ComboBox combEindDag;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Button btnOpslaan;
+        private System.Windows.Forms.DateTimePicker dateTimeBegin;
+        private System.Windows.Forms.DateTimePicker dateTimeEind;
     }
 }
